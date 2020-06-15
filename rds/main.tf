1d2b719ee26700c59eb7b25b76da20548c037ee1
Guru, Raghavendra <raghu.guru@incashme.com>
2020-06-15 14:36:20 +0530

    first cut

---
+++ rds/main.tf
@@ -0,0 +1,103 @@
+data "aws_availability_zones" "available" {}
+
+resource "aws_db_subnet_group" "default" {
+  name       = "rds-db-group"
+  subnet_ids = var.db_subnets
+
+  tags = {
+    Name = "${var.cluster_identifier} DB subnet group"
+    environment = var.envt
+  }
+}
+
+resource "aws_rds_cluster" "rdcluster" {
+  cluster_identifier      = var.cluster_identifier
+  engine                  = var.engine
+  engine_version          = var.engine_version
+
+  availability_zones      = data.aws_availability_zones.available.names
+  
+  db_subnet_group_name    = aws_db_subnet_group.default.id
+  vpc_security_group_ids  = var.db_secgrps
+
+  database_name           = var.dbname
+  master_username         = var.dbuser
+  master_password         = var.dbpwd
+  backup_retention_period = 5
+  preferred_backup_window = "07:00-09:00"
+  // skip_final_snapshot       = true
+  final_snapshot_identifier = var.final_snapshot
+
+  snapshot_identifier = var.base_snapshot
+
+  apply_immediately = true
+  storage_encrypted = false
+
+  tags = {
+    environment = var.envt
+  }
+}
+
+// resource "aws_rds_cluster_instance" "writer" {
+//   cluster_identifier  = aws_rds_cluster.rdcluster.id
+//   identifier          = "rdcluster-writer"
+//   engine              = aws_rds_cluster.rdcluster.engine
+//   engine_version      = aws_rds_cluster.rdcluster.engine_version
+//   // availability_zones  = ["ap-south-1a", "ap-south-1b", "ap-south-1c"]
+//   publicly_accessible = true
+//   apply_immediately   = true
+//   performance_insights_enabled = true
+
+//   instance_class     = "db.r4.large"
+// }
+
+resource "aws_rds_cluster_instance" "rdsinst" {
+  count               = var.instance_count
+  identifier          = "${var.inst_identifier}-${count.index}"
+
+  cluster_identifier  = aws_rds_cluster.rdcluster.id
+  engine              = aws_rds_cluster.rdcluster.engine
+  engine_version      = aws_rds_cluster.rdcluster.engine_version
+
+  // availability_zones  = ["ap-south-1a", "ap-south-1b", "ap-south-1c"]
+ //  publicly_accessible = true
+
+  apply_immediately   = true
+  performance_insights_enabled = true
+
+  instance_class     = var.instance_class
+   
+  tags = {
+    environment = var.envt
+  }  
+}
+
+
+output "rds_endpoint" {
+  value = aws_rds_cluster_instance.rdsinst.*.endpoint
+}
+
+output "rds_endpoint_iswriter" {
+  value = aws_rds_cluster_instance.rdsinst.*.writer
+}
+
+
+variable "cluster_identifier" {}
+variable "inst_identifier"    {}
+variable "engine"             {}
+variable "engine_version"     {}
+
+variable "dbname"             {}
+variable "dbuser"             {}
+variable "dbpwd"              {}
+
+variable "base_snapshot"      {}
+variable "final_snapshot"     {}
+variable "instance_count"     {}
+variable "instance_class"     {} 
+
+variable "db_subnets"         { type=list(string) }  
+variable "db_secgrps"         { type=list(string) }  
+
+variable "envt"               {} 
+
---
+++ vpsec/main.tf
@@ -0,0 +1,98 @@
+data "aws_availability_zones" "available" {}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "2.6.0"
+
+  name                 = var.vpc_name
+  cidr                 = var.cidr
+  azs                  = data.aws_availability_zones.available.names
+  
+  private_subnets     = var.private_subnets
+  public_subnets      = var.public_subnets
+  
+  
+  enable_nat_gateway   = var.enable_nat 
+  single_nat_gateway   = true
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+  enable_s3_endpoint   = true
+  enable_dynamodb_endpoint = true
+
+  tags = {
+    "kubernetes.io/cluster/${var.kcluster_name}" = "shared"
+    "environment" = var.envt
+  }
+
+  public_subnet_tags = {
+    "kubernetes.io/cluster/${var.kcluster_name}" = "shared"
+    "kubernetes.io/role/elb"                      = "1"
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/cluster/${var.kcluster_name}" = "shared"
+    "kubernetes.io/role/internal-elb"             = "1"
+  }
+}
+
+
+
+resource "aws_security_group" "ssh_access" {
+  name_prefix = "ssh_access"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
+    cidr_blocks = [
+      "10.0.0.0/8"
+    ]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "db_jump_access" {
+  name_prefix = "db_jump_access"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    from_port = 5432
+    to_port   = 54322
+    protocol  = "tcp"
+    cidr_blocks = [
+      var.jumpbox
+    ]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
+
+output "vpc_id"         { value = module.vpc.vpc_id }
+output "sg_ssh_id"      { value = aws_security_group.ssh_access.id }
+output "sg_dbjump_id"   { value = aws_security_group.db_jump_access.id }
+
+output "public_subnets"     { value = module.vpc.public_subnets }
+output "private_subnets"     { value = module.vpc.private_subnets }
+
+
+variable "vpc_name"        {}
+variable "cidr"            {}
+variable "public_subnets"  { type=list(string) }
+variable "private_subnets" { type=list(string) }
+variable "enable_nat"      { type=bool }
+variable "kcluster_name"   {}
+variable "envt"            {}
+variable "jumpbox"         {}
